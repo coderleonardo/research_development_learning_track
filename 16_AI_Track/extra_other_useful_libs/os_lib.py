8587f0d2cda816008463f540fa538762d7361cff
Leonardo Cesar <coderleonardo@gmail.com>
2023-01-08 11:04:07 -0300

    Removing cache

--- 16_AI_Track/extra_other_useful_libs/os_lib.py
+++
@@ -1,2 +0,0 @@
-# This module provides a portable way of using operating system dependent functionality.
-# https://docs.python.org/3/library/os.html
--- 16_AI_Track/extra_other_useful_libs/pickle_lib.py
+++
@@ -1,2 +0,0 @@
-# pickle — Python object serialization
-# https://docs.python.org/3/library/pickle.html
